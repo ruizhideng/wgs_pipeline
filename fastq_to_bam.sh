e1c5da5d7f206a24dbcbcd0752ea38cb6f0b8931
ruizhideng <58208888+ruizhideng@users.noreply.github.com>
2020-03-30 08:53:54 +0200

    Add files via upload

--- fastq_to_bam.sh
+++ fastq_to_bam.sh
@@ -1,11 +1,11 @@
 # command-line arguments, my work_dir = /home/ruizhi/data/wgs_pipeline/
-# fastq raw data is stored at ${work_dir}/test_data
+# fastq raw data is stored at ${work_dir}/fasta_file
 # assume your fastq filename format is: SAMPLE-ID_*.1.fq.gz
 work_dir=$1
 
 #reference
 reference=/home/ruizhi/data/wgs_pipeline/reference/genome/hg38.fa
-GATK_bundle=/home/ruizhi/data/wgs_pipeline/resources/gatk/bundle
+GATK_bundle=/home/ruizhi/data/wgs_pipeline/resources/gatk/bundle/hg38
 # create result folders
 if [ ! -d ${work_dir}/result/qc ]
 then mkdir -p ${work_dir}/result/qc
@@ -23,17 +23,21 @@ if [ ! -d ${work_dir}/result/gatk ]
 then mkdir -p ${work_dir}/result/gatk
 fi
 
+if [ ! -d ${work_dir}/result/gatk/VCF ]
+then mkdir -p ${work_dir}/result/gatk/VCF
+fi
+
 # fastqc: check fastq files quality, result stored at result/qc 
 echo "fastqc checking data quality started at $(date)" 
 cd ${work_dir}/result/qc
-fastqc ${work_dir}/test_data/*gz -o ${work_dir}/result/qc
+fastqc ${work_dir}/fasta_file/*gz -o ${work_dir}/result/qc
 echo "fastqc checking data quality finished at $(date)" 
 
 # filter low_quality data and remove adapter
 cd ${work_dir}/result/cleanfq
 echo "trim_galore cutting adaptors started at $(date)" 
-ls ${work_dir}/test_data/*1.fq.gz > ./1;cat 1
-ls ${work_dir}/test_data/*2.fq.gz > ./2;cat 2
+ls ${work_dir}/fasta_file/*1.fq.gz > ./1;cat 1
+ls ${work_dir}/fasta_file/*2.fq.gz > ./2;cat 2
 paste 1 2 > config && rm 1 2
 cat ${work_dir}/result/cleanfq/config | while read id; do 
     arr=(${id}); fq1=${arr[0]}; fq2=${arr[1]}; trim_galore -q 25 --phred33 --length 36 -e 0.1 --stringency 3 --paired $fq1 $fq2 -o ${work_dir}/result/cleanfq
@@ -53,7 +57,7 @@ echo "bwa mapping reads finished at $(date)" && rm config
 
 # sort bam files
 echo "samtools sort sorting bam files started at $(date)"
-ls *.bam | while read id; do samtools sort -@ 4 -m 4G -O bam -o ${work_dir}/result/bwa/`basename $id .bam`.sorted.bam $id;done
+ls *.bam | while read id; do samtools sort -@ 4 -m 90M -O bam -o ${work_dir}/result/bwa/`basename $id .bam`.sorted.bam $id;done
 echo "samtools sort sorting bam files finished at $(date)"
 
 # build index for bam files
--- preparation.sh
+++ preparation.sh
@@ -3,10 +3,12 @@
 # my work_dir=/home/ruizhi/data/wgs_pipeline
 # fastq raw data is stored at ${work_dir}/test_data
 work_dir=$1
+reference=/home/ruizhi/data/wgs_pipeline/reference/genome
+GATK_bundle=/home/ruizhi/data/wgs_pipeline/resources/gatk/bundle/hg38
 cd ${work_dir}
 
-if [ ! -d ${work_dir}/test_data ]
-then mkdir -p ${work_dir}/test_data
+if [ ! -d ${work_dir}/fasta_file ]
+then mkdir -p ${work_dir}/fasta_file
 fi
 
 if [ ! -d ${work_dir}/reference/genome ]
@@ -21,30 +23,19 @@ if [ ! -d ${work_dir}/resources/gatk/bundle ]
 then mkdir -p ${work_dir}/resources/gatk/bundle 
 fi
 
-#download test data
-cd ${work_dir}/test_data
-nohup wget -c -r -nd -np -k -L -p ftp://biodisk.org/Release/KPGP/KPGP_Data_2015_Release_Candidate/WES/KPGP-00245/ 1>/dev/null 2>&1 &
+# download test data
+##cd ${work_dir}/fasta_file
+##nohup wget -c -r -nd -np -k -L -p ftp://biodisk.org/Release/KPGP/KPGP_Data_2015_Release_Candidate/WES/KPGP-00245/ 1>/dev/null 2>&1 &
 
-# download reference GRCh38
-cd ${work_dir}/reference/genome
-nohup wget http://hgdownload.cse.ucsc.edu/goldenPath.hg38/bigZips/hg38.fa.gz &
-gzip hg38.fa.gz
-
-# download gatk bundle hg38
-# ftp://ftp.broadinstitute.org/bundle/hg38/
-cd ${work_dir}/resource/gatk/bundle
-nohup wget ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/hg38/dbsnp_146.hg38.vcf.gz &
-nohup wget ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/hg38/dbsnp_146.hg38.vcf.gz.tbi & 
-nohup wget ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/hg38/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz & 
-nohup wget ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/hg38/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz.tbi & 
-nohup wget ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/hg38/Homo_sapiens_assembly38.fasta.gz & 
-nohup wget ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/hg38/Homo_sapiens_assembly38.fasta.fai & nohup wget ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/hg38/Homo_sapiens_assembly38.dict & 
-nohup wget ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/hg38/1000G_phase1.snps.high_confidence.hg38.vcf.gz & 
-nohup wget ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/hg38/1000G_phase1.snps.high_confidence.hg38.vcf.gz.tbi &
-
-
-# build reference index
-cd ${work_dir}/index/bwa
-nohup time bwa index -a bwtsw -p /home/ruizhi/data/wgs_pipeline/reference/genome/hg38.fa /home/ruizhi/data/wgs_pipeline/reference/genome/hg38.fa  1>hg38.bwa_index.log 2>&1 &
+# download reference GRCh38 and build reference index
+cd $reference
+wget http://hgdownload.cse.ucsc.edu/goldenPath.hg38/bigZips/hg38.fa.gz && gunzip hg38.fa.gz
+bwa index -a bwtsw $reference/hg38.fa
   
-
+# download gatk bundle hg38 from ftp-sites
+# ftp://ftp.broadinstitute.org/bundle/hg38/
+# username: gsapubftp-anonymous
+# password:
+# build GATK_bundle index
+cd $GATK_bundle
+gunzip $GATK_bundle/Homo_sapiens_assembly38.fasta.gz
