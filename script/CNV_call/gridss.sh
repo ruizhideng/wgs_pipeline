02c7cb98510e89d212dd14a52cd140cdccde1cec
ruizhideng <58208888+ruizhideng@users.noreply.github.com>
2020-04-21 16:54:13 +0200

    Add files via upload

--- script/CNV_call/gridss.sh
+++ script/CNV_call/gridss.sh
@@ -1,4 +1,5 @@
 # GRIDSS CNV tools
+# -t meanns threads
 work_dir=$1
 gridss=$2 #/PATH/TO/gridss/ like /home/ruizhi/data/biosoft/gridss/, gridss installed directory
 reference=${work_dir}/reference/genome/hg38.fa
@@ -11,11 +12,12 @@ echo "gridss calling CNV started at $(date)"
 cd ${work_dir}/result/gatk/
 ls *sorted.markdup.bam| while read id; do
     ${gridss}/gridss.sh  \
+    -t 1 \
     -r ${reference} \
     -o ${work_dir}/result/CNV/gridss/`basename ${id} .sorted.markdup.bam`.vcf.gz \
-    -a ${id} -j ${gridss}/gridss-2.8.3-gridss-jar-with-dependencies.jar \
+    -a ${work_dir}/result/CNV/gridss/`basename ${id} .sorted.markdup.bam`.assembly.bam \
+    -j ${gridss}/gridss-2.8.3-gridss-jar-with-dependencies.jar \
     -w ${work_dir}/result/CNV/gridss \
     --jvmheap 4g \
-    $id
-done
+    $id;done
 echo "gridss calling CNV finished at $(date)"
